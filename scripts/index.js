e3a5a9a4942a1b9f730f61c7d977167512026468
BrianD <briandoan44@gmail.com>
2022-12-13 17:55:08 -0800

    javascript

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+const initialCards = [
+    {
+        name: "Yosemite Valley",
+        link: "https://images.unsplash.com/photo-1593209984201-16511667bd12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    },
+
+    {
+        name: "Lake Louise",
+        link: "https://images.unsplash.com/photo-1516293635722-a0ae2709d570?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1738&q=80",
+    },
+
+    {
+        name: "Bald Mountains",
+        link: "https://plus.unsplash.com/premium_photo-1661823662989-9d441b95d906?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1848&q=80",
+    },
+
+    {
+        name: "Latemar",
+        link: "https://images.unsplash.com/photo-1539540706191-b673a0a5b6d6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    },
+
+    {
+        name: "Vanoise National Park",
+        link: "https://images.unsplash.com/photo-1488810050923-3897e8aa9037?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2296&q=80",
+    },
+
+    {
+        name: "Lago di Braies",
+        link: "https://images.unsplash.com/photo-1583080652438-cd43f723205f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80",
+    },
+];
+
+
+
+
+
